5ff8af601db16eff2749c26540e4e249156f166e
yarenfidan <yarenfidan10@gmail.com>
2018-03-20 12:39:33 +0300

    Add files via upload

--- katmaDegerCiro-1.py
+++ katmaDegerCiro-1.py
@@ -1,15 +1,26 @@
-x=int(input("Toplam Satış Miktarını Giriniz:"))
-y=int(input("Hammadde Maliyetini Giriniz:"))
-z=int(input("Bakım Onarım Giderlerini Giriniz:"))
-t=int(input("Sevkiyat Giderlerini Giriniz:"))
-f=int(input("Satın Alınan Hizmet Giderlerini Giriniz:"))
-def KatmaDegerCiroFonk():
-    KatmaDegerCiro=x-(y+z+t+f)
-    return KatmaDegerCiro
-if KatmaDegerCiroFonk()>=1000:
-    print("İşletme katma değer cirosu yüksek")
-elif KatmaDegerCiroFonk()<500:
-    print("işletme katma değer cirosu düşük")
-else:
-    print("işletme katma değer cirosu normal")
+toplamSatis=int(input(" Toplam Satış miktarını giriniz: "))
+hammaddeMaliyet=int(input("Hammadde maliyetini giriniz: "))
+bakimOnarim=int(input("Bakım Onarım Giderlerini giriniz: "))
+sevkiyatGider=int(input("Sevkiyat Giderlerini giriniz: "))
+satinAlinanHizmet=int(input("Satın Alınan Hizmet Giderlerini giriniz: "))
 
+def giderHesapla(hammaddeMaliyet,bakimOnarim,sevkiyatGider,satinAlinanHizmet):
+    global toplamGider
+    toplamGider=hammaddeMaliyet+bakimOnarim+sevkiyatGider+satinAlinanHizmet
+    return toplamGider
+
+def KatmaDegerCiroHesapla(toplamSatis,toplamGider):
+    katmaDegerCiro=toplamSatis-toplamGider
+    print(katmaDegerCiro)
+
+    if(katmaDegerCiro>=1000):
+        print("İşletme katma değer cirosu yüksek")
+    elif(500<=katmaDegerCiro<=999):
+        print("işletme katma değer cirosu normal")
+
+    else:
+        print("işletme katma değer cirosu düşük")
+
+gelir=toplamSatis
+gider=giderHesapla(hammaddeMaliyet,bakimOnarim,sevkiyatGider,satinAlinanHizmet)
+KatmaDegerCiroHesapla(toplamSatis,toplamGider)
